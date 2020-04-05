45f892d5cf47d2c201b9e73722776b13f327e823
Maria Idrissi Kaitouni <maria.idrissi-kaitouni@etu.utc.fr>
2020-04-05 18:14:43 +0200

    Update resilience.sql

--- v1/resilience.sql
+++ v1/resilience.sql
@@ -96,24 +96,33 @@ INSERT INTO Compte VALUES
 ('AhAnDkoiJNiHNhY3s5dBorCnDoFB5ojexuxZdzempooK'),
 ('kjNhJhbjhkiUuiu3s5dBorCnDoFB5hbjhkiUuiuszoaZ'),
 ('orCnDoFB5ojexuxZdzempooKAhAnDkoiJNiHNhY3s5dB'),
-('orCnDoRFZspgrnlmzaRFZspgrnlmzaKAhAnDkoY3s5dB')
+('fhGRKEI45fsdDdEF43sF45TESEfEF43sF45TESEffjns'),
+('mzaefeUNiJmCnDoFB5hbjhkiUuiuszfhbjhkiUuiuszf'),
+('eked34nzeukdjEDJfhj45JZnejkdh3445ERFskdhkedf')
 ;
 
 INSERT INTO Personne(pseudo, prenom, nom, dateNaissance, longitude, latitude, compte) VALUES
   ('matt', 'Matthieu', 'GLORION', '1997-10-24', 47.390547, -2.955815, '8qQ7UhikRFZspgrnlmzaefeUNiJmAWmmSomuiomyui1x'),
   ('clementdupuis', 'Clément', 'DUPUIS', '1997-10-24', 49.415048, 2.818973, 'AhAnDkoiJNiHNhY3s5dBorCnDoFB5ojexuxZdzempooK'),
   ('mariaidrissi', 'Maria', 'IDRISSI', '1997-10-24', 49.401488, 2.801639, 'kjNhJhbjhkiUuiu3s5dBorCnDoFB5hbjhkiUuiuszoaZ'),
-  ('pilo', 'Pilo', 'MILIEU', '1997-10-24', 49.408485, 2.808484, 'orCnDoFB5ojexuxZdzempooKAhAnDkoiJNiHNhY3s5dB'),
-  ('bryan', 'Nicolas', 'CALMELS', '1900-01-01', 49.408485, 2.808484, 'orCnDoRFZspgrnlmzaRFZspgrnlmzaKAhAnDkoY3s5dB')
+  ('pilo', 'Pilo', 'MILIEU', '1997-10-24', 49.408485, 2.808484, 'orCnDoFB5ojexuxZdzempooKAhAnDkoiJNiHNhY3s5dB')
 ;
 
-INSERT INTO LienPersonne(description, personneDeclarant, personneConcernee) VALUES
-  ('collaboration NA17', 'matt', 'mariaidrissi'),
-  ('collaboration NA17', 'matt', 'clementdupuis'),
-  ('collaboration NA17', 'clementdupuis', 'mariaidrissi'),
-  ('collaboration NA17', 'clementdupuis', 'matt'),
-  ('collaboration NA17', 'mariaidrissi', 'matt'),
-  ('collaboration NA17', 'mariaidrissi', 'clementdupuis'),
-  ('voisin', 'matt', 'pilo'),
-  ('chef de grafhit', 'bryan', 'matt')
+
+INSERT INTO Communaute(nom,dateCreation, description, longitude, latitude, compte) VALUES 
+  ('Vegancommunaute', '2020-03-04', 'Cette communauté est déstinée à toute personne végane ou intéréssée par un mode de vie végan', 59.4, 45.34, 'fhGRKEI45fsdDdEF43sF45TESEfEF43sF45TESEffjns'),
+  ('Yogacommunaute', '2020-02-04', 'Cette communauté est déstinée à toute personne pratiquant ou intéréssée par le yoga', 65.4, 75.34, 'mzaefeUNiJmCnDoFB5hbjhkiUuiuszfhbjhkiUuiuszf')
+  ('Jardinagecommunaute', '2020-02-04', 'Cette communauté est déstinée à toute personne pratiquant ou intéréssée par le jardinage', 34.4, 29.34, 'eked34nzeukdjEDJfhj45JZnejkdh3445ERFskdhkedf')
+  ;
+
+INSERT INTO LienCommunaute(description, CommunauteQuiDeclare, CommunauteConcernee) VALUES
+('Les gens de la Vegancommunaute peuvent faire des séances de yoga avec la Yogacommunaute','Vegancommunaute','Yogacommunaute'),
+('Les gens de la Yogacommunaute peuvent faire des séances de jardinage avec la Jardinagecommunaute','Yogacommunaute','Jardinagecommunaute'),
+('Les gens de la Jardinagecommunaute peuvent donner des légumes à la Vegancommunaute','Jardinagecommunaute','Vegancommunaute')
+;
+
+INSERT INTO PersonneFaitPartieCommunaute(personne, communaute, exclu) VALUES
+('matt', 'Jardinagecommunaute', '0'),
+('mariaidrissi', 'Yogacommunaute', '0'),
+('clementdupuis', 'Vegancommunaute', '0'),
 ;
